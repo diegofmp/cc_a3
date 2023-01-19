61b7abff381d341501d8d4eb32dc0865fbdbd959
Monge Pimentel Diego Fernando <a12044031@unet.univie.ac.at>
2023-01-19 13:38:02 +0100

    Implementation of dictionary operations

--- services/api/app.py
+++ services/api/app.py
@@ -1,14 +1,177 @@
 import flask
 from flask import request, jsonify, make_response, abort
 from datetime import datetime
+import requests
+import os
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+# setup
+database_url = os.getenv("DATABASE-SVC", "http://database")
+
+# internal functions ###########################################################
+num_buckets = 4
+'''
+input: key
+output: target database bucket id
+
+Using the modulo operator it calculates the target database's bucket
+'''
+def hash_function(key):
+    return key % num_buckets
+
+
+def getTime():
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+def log(something):
+    app.logger.info(something)
+
 
 ####################### Endpoints ##############################################
 @app.route("/test",  methods = ['GET'])
 def test_endpoint():
     return "Grettings from API service!"
+
+
+@app.route("/insert",  methods = ['POST'])
+#@expects_json(frame_schema)
+def insert():
+    # take the key and value a from request
+    req = request.get_json()
+
+    key = req["k"]
+    a = req["a"]
+
+
+    # Process: get target key
+    target_bucket = hash_function(key)
+
+    # Send to database bucket
+    # prepare request 
+    db_request = {
+        "key" : key,
+        "value": a,
+        "target": target_bucket                
+    }
+
+    log("DB_REQUEST: ")
+    log(db_request)
+    
+    # send request
+    response = requests.post(database_url + "/insert", json=db_request)
+    json_response = response.json()
+    log("JSON_RESPONSE OF DB: ")
+    log(json_response)
+
+    response = {
+        "timestamp": getTime(),
+    }
+    
+    return response
+
+@app.route("/range",  methods = ['GET'])
+def range_search():
+    k1 = request.args.get('k1') # from
+    k2 = request.args.get('k2') # to
+
+    
+    k1 = int(k1)
+    k2 = int(k2)
+
+    # validate k1 and k2
+    if(k2 < k1 or k1< 0 or k2<0):
+        message = "Invalid params!. Consider that: K1 and K2 shold be positive numbers. K2 must be greater or equal than K1"
+        abort(make_response(jsonify(message=message), 400))
+
+    log("[API] range k: ")
+    log(k1)
+    log(k2)
+
+    k_s = [k for k in range(k1, k2+1)] # list all keys in range
+
+    buckets = [hash_function(k) for k in k_s] # calculate buckets for each
+
+    # prepare request 
+    db_request = {
+        "keys" : k_s,
+        "buckets": buckets
+    }
+
+    log("DB_REQUEST: ")
+    log(db_request)
+    
+    # send request
+    response = requests.post(database_url + "/range",  json=db_request)
+    json_response = response.json()
+    log("JSON_RESPONSE OF DB: ")
+    log(json_response)
+
+    if(json_response.get("contents")):
+        return json_response
+    else:
+        error_type = json_response.get("error_type")
+        message = json_response.get("message")
+        abort(make_response(jsonify(message=message, error_type=error_type), 400))
+
+@app.route("/search/<k>",  methods = ['GET'])
+def search(k):
+    log("[API] searching k: ")
+    log(k)
+    k = int(k)
+    log("[API] searching k: ")
+    log(k)
+
+    bucket = hash_function(k)
+
+    # prepare request 
+    db_request = {
+        "key" : k,
+        "bucket": bucket
+    }
+
+    log("DB_REQUEST: ")
+    log(db_request)
+    
+    # send request
+    response = requests.get(database_url + "/search", params=db_request)
+    json_response = response.json()
+    log("JSON_RESPONSE OF DB: ")
+    log(json_response)
+
+    if(json_response.get("content")):
+        return json_response
+    else:
+        error_type = json_response.get("error_type")
+        message = json_response.get("message")
+        abort(make_response(jsonify(message=message, error_type=error_type), 400))
+
+@app.route("/delete/<k>",  methods = ['DELETE'])
+def delete(k):
+    k = int(k)
+    bucket = hash_function(k)
+
+    # prepare request 
+    db_request = {
+        "key" : k,
+        "bucket": bucket
+    }
+
+    # send request
+    response = requests.delete(database_url + "/delete", params=db_request)
+    json_response = response.json()
+    log("JSON_RESPONSE OF DB: ")
+    log(json_response)
+
+    if response.ok:
+        return json_response
+    else:
+        error_type = json_response.get("error_type")
+        message = json_response.get("message")
+        abort(make_response(jsonify(message=message, error_type=error_type), 400))
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
--- services/database/app.py
+++ services/database/app.py
@@ -1,14 +1,214 @@
 import flask
 from flask import request, jsonify, make_response, abort
 from datetime import datetime
+import os
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+## General params
+num_buckets = 4
+root_buckets_path = os.path.join(os.getcwd(), 'buckets')
+
+
+### UTILS
+def getTime():
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+def log(something):
+    app.logger.info(something)
+
+## initial check of the main folder
+def check_root_directory():
+    log("--------- check root directory....")
+    target_dir = os.path.join(os.getcwd(), 'buckets')
+    log("TARGET_DIR: ")
+    log(target_dir)
+
+    if not os.path.exists(target_dir):
+        
+        try:
+            log("folder doesmnt exsit!!!!. gonna create")
+
+            # create root folder and subfolders (buckets)
+            for i in range(1, num_buckets+1):
+                target_subdir = os.path.join(target_dir, str(i))
+                log(target_subdir)
+                os.makedirs(target_subdir)
+        except Exception as e:
+            log("Error creating folder")
+            log(e)
+    else:
+        log("Root folder already exists!!")
+        
+        
+        
 
 ####################### Endpoints ##############################################
 @app.route("/test",  methods = ['GET'])
 def test_endpoint():
     return "Grettings from DATABASE service!"
 
+@app.route("/search",  methods = ['GET'])
+def search():
+    key = request.args.get('key')
+    bucket = request.args.get('bucket')
+
+    try:
+        log("Trying to read....")
+        filename = os.path.join(root_buckets_path, str(bucket), str(key))
+        log(filename)
+        file = open(filename, mode='r')
+        content = file.read()
+        log(content)
+        file.close()
+
+        response = {
+            "content": content,
+            "timestamp": getTime(),
+        }
+        
+        return response
+
+    except FileNotFoundError as e:
+        error_message = "Key not found!"
+        log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR: file not found!")
+        log(e)
+        abort(make_response(jsonify(message=error_message, error_type="not_found"), 400))
+        
+    except Exception as e: # General error
+        error_message = "An exception occurred"
+        log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR reading")
+        log(e)
+        abort(make_response(jsonify(message=error_message, error_type="internal_error"), 400))
+
+
+
+
+@app.route("/insert",  methods = ['POST'])
+#@expects_json(frame_schema)
+def insert():
+    try:
+        log(">>>>>>>>>>>> init insert....")
+        # take the key and value a from request
+        req = request.get_json()
+
+        key = req["key"] # will be the filename
+        value = req["value"] # content
+        target = req["target"] # bucket id
+
+        log(".....>> gonna try to store!")
+
+        # insert file into corresponding target folder
+        # TODO
+        with open(os.path.join(root_buckets_path, str(target), str(key)), 'w') as fp:
+            fp.write(value)
+            
+        log("STORING....")
+        log("request: ")
+        log(req)
+
+        response = {
+            "timestamp": getTime(),
+        }
+        
+        return response
+
+    except Exception as e:
+        error_message = "An exception occurred"
+        log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
+        log(e)
+        abort(make_response(jsonify(message=error_message), 400))
+
+    response = {
+        "timestamp": getTime(),
+    }
+    
+    return response
+
+
+@app.route("/range",  methods = ['POST'])
+def range_search():
+    req = request.get_json()
+
+    keys = req["keys"]
+    buckets = req["buckets"]
+    log("Keys: ")
+    log(keys)
+    log("Buvkets: ")
+    log(buckets)
+
+    contents = []
+    for item in list(zip(keys, buckets)):
+        log(item)
+        key = int(item[0])
+        bucket = int(item[1])
+
+        try:
+            log("Trying to read....")
+            filename = os.path.join(root_buckets_path, str(bucket), str(key))
+            log(filename)
+            file = open(filename, mode='r')
+            content = file.read()
+            log(content)
+            file.close()
+
+            content_item = {
+                "key": key,
+                "content": content
+            }
+            contents.append(content_item)
+
+        except FileNotFoundError as e:
+            error_message = "Key not found!"
+            log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR: file not found!")
+            log(e)
+            pass
+            #abort(make_response(jsonify(message=error_message, error_type="not_found"), 400))
+            
+        except Exception as e: # General error
+            error_message = "An exception occurred"
+            log("Internal error!!! stop")
+            log(e)
+            abort(make_response(jsonify(message=error_message, error_type="internal_error"), 400))
+
+    response = {
+        "contents": contents,
+        "timestamp": getTime(),
+    }
+
+    return response
+
+
+@app.route("/delete",  methods = ['DELETE'])
+def delete():
+    key = request.args.get('key')
+    bucket = request.args.get('bucket')
+
+    try:
+        log("Trying to delete....")
+        filename = os.path.join(root_buckets_path, str(bucket), str(key))
+        log(filename)
+        if os.path.isfile(filename):
+            os.remove(filename)
+        else:
+            error_message = "Key not found!"
+            abort(make_response(jsonify(message=error_message), 400))
+        
+        response = {
+            "removed": key,
+            "timestamp": getTime(),
+        }
+        
+        return response
+
+    except Exception as e: # General error
+        error_message = "An exception occurred"
+        log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR deleting")
+        log(e)
+        abort(make_response(jsonify(message=error_message, error_type="internal_error"), 400))
+
+
 if __name__ == '__main__':
+    check_root_directory()
     app.run(host="0.0.0.0", port=80)
